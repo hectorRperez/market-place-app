02cd2026f990d3769c91d7bf614dc873a7ddff1a
Hector Perez <hectorperez1238@gmail.com>
2022-03-27 13:02:47 -0400

    first commit

---
+++ src/view/HomePage.vue
@@ -0,0 +1,72 @@
+<template>
+  <h1>hola desde homepaga</h1>
+</template>
+
+<script>
+import "../assets/styles/main.css";
+
+export default {};
+</script>
+
+<style>
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  margin: var(--header-heigth) 0 0 0;
+  font-family: var(--body-font);
+  font-size: var(--normal-font-size);
+  font-weight: var(--font-medium);
+  color: var(--dark-color);
+}
+
+h1,
+h2,
+h3,
+p,
+ul {
+  margin: 0;
+}
+
+ul {
+  padding: 0;
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+  color: var(--dark-color);
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+}
+
+/*==== CLASS CSS ==========*/
+.section {
+  padding: 5rem 0 2rem;
+}
+
+.section-title {
+  position: relative;
+  font-size: var(--big-font-size);
+  margin-bottom: var(--mb-4);
+  text-align: center;
+  letter-spacing: 0.1rem;
+}
+
+.section-title::after {
+  content: "";
+  position: absolute;
+  width: 56px;
+  height: 0.18rem;
+  top: -1rem;
+  left: 0;
+  right: 0;
+  margin: auto;
+  background-color: var(--dark-color);
+}
+</style>
